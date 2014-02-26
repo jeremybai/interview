14cb8661ba245ba874ed41896e1179cff4cacaf7
jeremybai <jeremybaiit@gmail.com>
2014-02-26 12:59:03 +0800

    添加插入排序和快速排序

--- 02_经典题目/排序/Sort.c
+++ 02_经典题目/排序/Sort.c
@@ -20,8 +20,8 @@ static void Swap(int *a, int *b)
 }
 /** 
  * @brief     冒泡排序函数实现从小到大排序。
- * @param[in] a  数组起始地址
- * @param[in] n  数组大小
+ * @param[in] array  数组起始地址
+ * @param[in] length  数组大小
  * @retval    None
  * @see       None
  * @note      时间复杂度O(n^2)；最好情况O(n)；最坏情况O(n^2)；
@@ -56,8 +56,8 @@ int BubleSort(int *array, int length)
 
 /** 
  * @brief     插入排序函数实现从小到大排序。
- * @param[in] a  数组起始地址
- * @param[in] n  数组大小
+ * @param[in] array  数组起始地址
+ * @param[in] length  数组大小
  * @retval    None
  * @see       None
  * @note      时间复杂度O(n^2)；最好情况O(n)；最坏情况O(n^2)；
@@ -86,6 +86,46 @@ int InsertSort(int *array, int length)
 	return 0;
 }
 
+/** 
+ * @brief     快速排序函数实现从小到大排序。
+ * @param[in] array  数组起始地址
+ * @param[in] length  数组大小
+ * @retval    None
+ * @see       None
+ * @note      时间复杂度O(n^)；最好情况O()；最坏情况O(n^)；
+			  空间复杂度O()；稳定。			  
+ */
+void QuickSort(int *array, int low,int high)
+{
+	if((NULL == array) || low < 0 || high < 0)
+	{
+		printf("输入参数有误\n");
+		return 1;
+	}
+	if ( low < high ) 
+	{
+		int i = low, j = high, temp = array[low];
+		while ( i < j ) 
+		{
+			//从右往左扫描，如果数组元素大于temp，则继续，直至找到第一个小于temp的元素
+			while ( i < j && array[j] >= temp ) {
+				--j;
+			}
+			if ( i < j ) {
+				array[i++] = array[j];
+			}
+			while ( i < j && array[i] <= temp ) {
+				++i;
+			}
+			if ( i < j ) {
+				array[j--] = array[i];
+			}
+		}
+		array[i] = temp;
+		QuickSort(array, low, i - 1);
+		QuickSort(array, i + 1, high);
+	}
+}
 
 
 
@@ -95,11 +135,9 @@ int main()
 	int ForSort[10] = {9,1,8,2,7,3,4,6,5,0},i;
 	int ForSort1[10] = {0,1,2,3,4,5,6,7,8,9};
 	int ForSort2[10] = {9,8,7,6,5,4,3,2,1,0};
-	if(0 == InsertSort(ForSort2,10))
-	{	
-		for(i = 0; i < 10; i++)
-		{
-			printf("%d   ",ForSort2[i]);
-		}
+	QuickSort(ForSort2,0,9);
+	for(i = 0; i < 10; i++)
+	{
+		printf("%d   ",ForSort2[i]);
 	}
 }
